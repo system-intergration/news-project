21d8ea67dd2d8f8288281777b79f2956daec6244
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2022-05-20 19:17:51 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/news-project/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
